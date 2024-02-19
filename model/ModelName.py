6d2d445db14a9418e7b49824d544db924ac53d60
erhanbarisolmez <61422236+erhanbarisolmez@users.noreply.github.com>
2024-02-19 16:06:03 +0300

    19.02.2024

--- model/ModelName.py
+++ model/ModelName.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, HttpUrl, Field, EmailStr
 from enum import Enum
 
 class ModelName(str, Enum):
@@ -6,10 +6,70 @@ class ModelName(str, Enum):
   resnet = "resnet"
   lenet = "lenet"
 
+class Image(BaseModel):
+  url:HttpUrl
+  name: str
+  
 
 class Car(BaseModel):
   name:str
+  description: str | None = Field(default= None, examples=["A black  car"])
+  price: float = Field(examples = [33.5])
+  tax: float | None = Field(examples= [3.2])
+  tags: set[str] = set()
+  image: list[Image] |None = None
+  
+
+class Offer(BaseModel):
+  name: str
   description: str | None = None
   price: float
-  tax: float | None = None
+  items: list[Car]
+  
+class BaseUser(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: str | None = None
 
+
+class UserIn(BaseUser):
+    password: str
+
+class Item(BaseModel):
+  name: str
+  description: str | None = None
+  price: float
+  tax: float = 10.5
+  tags: list[str] = [] 
+  
+
+class BaseProduct(BaseModel):
+  username: str
+  email: EmailStr
+  full_name:str | None = None
+   
+  
+
+class ProductIn(BaseProduct):
+  password: str
+  
+class ProductInDB(BaseProduct):
+  hashed_password:str
+  
+
+
+class CarBaseItem(BaseModel):
+  description : str
+  type: str
+  
+class CarItem(CarBaseItem):
+  type: str= "car"
+  
+class PlaneItem(CarBaseItem):
+  type: str = "plane"
+  size: int
+  
+class lItem(BaseModel):
+  name:str
+  description: str
+  
--- root.py
+++ root.py
@@ -1,8 +1,8 @@
-from typing import Annotated
-from fastapi import FastAPI, Query, Path
-from model.ModelName import ModelName,Car
-
-
+from typing import Annotated, List, Union, Any
+from fastapi import FastAPI, Query, Path, Body, Header, Response, status, Form, File, UploadFile
+from fastapi.responses import JSONResponse, RedirectResponse
+from model.ModelName import *
+from pydantic import Field, BaseModel
 
 app = FastAPI()
 
@@ -144,3 +144,188 @@ async def read_r(
         results.update({"q": q})
     return results
   
+  
+
+@app.put("/put/{item_id}")
+async def update_item(item_id: int, item: Annotated[Car, Body(embed=True)]):
+    results = {"item_id": item_id, "item": item}
+    return results
+  
+class Item(BaseModel):
+  name: str
+  description: str | None = Field(
+    default=None, title="The description of the item", max_length=300
+  )
+  price: float = Field(gt=0, description="a")
+  tax: float | None = None
+  
+@app.put("/asd/{item_id}")
+async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
+  results = {"item_id": item_id, "item": item}
+  return results
+
+@app.put("/cars/{car_id}")
+async def update_car(car_id: int, item: Car):
+  results = {"car_id": car_id, "Car": item}
+  return results
+
+@app.put("/l/{item_id}")
+async def update_l(item_id: int, item: Car):
+  results = {"item_id": item_id, "item": item}
+  return results
+
+@app.post("/offers/")
+async def create_offer(offer: Offer):
+  return offer
+
+@app.post("/images/multiple/")
+async def create_multiple_images(images: list[Image]):
+  for image in images:
+    image.name
+  return images
+
+
+@app.post("/index-weights/")
+async def create_index_weights(weights: dict[int, float]):
+  return weights
+
+@app.get("/header/")
+async def read_items(user_agent: Annotated[str | None, Header()] = None):
+  return {"User-Agent" : user_agent}
+
+@app.get("/xtoken/")
+async def read_items(x_token: Annotated[list[str] | None, Header()] = None):
+  return {"X-Token values" : x_token}
+
+@app.post("/formdata/", response_model=Car)
+async def create_item(item: Car) -> Any:
+  return item
+
+@app.get("/formdata/", response_model=list[Car])
+async def read_items() -> Any:
+  return [
+    Car(name="BMW", price=52.9),
+    Car(name="Mercedes", price=47.13)
+  ]
+  
+@app.post("/Iuser")
+async def create_user(user:UserIn) -> BaseUser:
+  return user
+
+@app.get("/portal")
+async def get_portal(teleport:bool = False) -> Response:
+  if teleport:
+    return RedirectResponse(url="http://www.youtube.com")
+  return JSONResponse(content={"message": "Here's your interdimensional portal."})
+
+@app.get("/teleport")
+async def get_teleport() -> RedirectResponse:
+  return RedirectResponse(url="https://youtube.com")
+
+
+items = {
+  "foo": {"name": "Foo", "price": 50.2},
+  "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+  "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+}
+
+@app.get("/ttt/{item_id}", response_model=Item, response_model_exclude_unset=True)
+async def read_item(item_id: str):
+  return items[item_id]
+
+item = {
+  "foo": {"name": "Foo", "price": 50.2},
+  "bar": {"name": "Bar", "description": "The Bar Fighters", "price":55, "tax": 20.3},
+  "baz": {
+    "name": "Baz",
+    "description": "There goes my baz",
+    "price": 50.2,
+    "tax": 10.4,
+  }
+}
+
+@app.get(
+  "/items/{item_id}/name",
+  response_model=Item,
+  response_model_include={"name", "description"},
+)
+async def read_item_name(item_id: str):
+  return item[item_id]
+
+@app.get("/items/{item_id}/public", response_model=Item, response_model_exclude={"tax"})
+async def read_item_public_data(item_id: str):
+  return item[item_id]
+
+
+def fake_password_hasher(raw_password: str):
+  return "supersecret" + raw_password
+
+def fake_save_product(product_in: ProductIn):
+  hashed_password = fake_password_hasher(product_in.password)
+  product_in_db = ProductInDB(**product_in.model_dump(), hashed_password=hashed_password)
+  print("Product saved! ..not really")
+  return product_in_db
+
+@app.get("/product2/", response_model=BaseProduct)
+async def create_product(product_in: ProductIn) ->  ProductIn:
+  product_saved = fake_save_product(product_in)
+  return product_saved
+
+
+itemCar = {
+  "item1": {"description": "All my friends drive a low rider", "type": "car"},
+  "item2": {
+    "description": "Music is my aeroplane, it's my aeroplane",
+    "type": "plane",
+    "size": 5
+  }
+}
+
+@app.get("/pp/{item_id}", response_model=Union[PlaneItem, CarItem])
+async def read_item(item_id: str):
+  return itemCar[item_id]
+
+iteml = [
+  {"name" : "Foo", "description": "There comes my hero"},
+  {"name" : "Red", "description": "There comes my red"}
+]
+
+@app.get("/ll/", response_model=list[lItem])
+async def read_items():
+  return iteml
+
+@app.get("/keyword-weights/", response_model=dict[str, float])
+async def keyword_weight(text: str):
+  words = text.split()
+  return words
+
+@app.post("/itemsStatus/", status_code=status.HTTP_201_CREATED)
+async def create_item(name:str):
+  return {"name": name}
+
+
+@app.post("/login/")
+async def login(username: Annotated[str, Form()], password:Annotated[str, Form()]):
+  return {"username": username, "password": password}
+
+@app.post("/files/")
+async def create_file(files: Annotated[list[bytes], File(description="Multiple files as bytes")]):
+  return {"file_size": [len(file) for file in files]}
+
+@app.post("/uploadfiles/")
+async def create_upload_file(files: Annotated[ list[UploadFile], File(description="Multiple files as UploadFile")]):
+  return {"filename": [file.filename for file in files]}
+
+
+@app.post("/files2")
+async def create_file(
+  file: Annotated[bytes, File()],
+  fileb: Annotated[UploadFile, File()],
+  token: Annotated[str, Form()],
+):
+  return {
+    "file_size": len(file),
+    "token": token,
+    "fileb_content_type": fileb.content_type
+  }
+
